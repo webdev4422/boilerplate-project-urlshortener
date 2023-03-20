6fad415f737796235e9e4142b24b1d87e5fabbcd
user <user@gmail.com>
2023-03-20 07:53:00 +0200

    Use body-parser to parse request as text

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+// Add body-parser
+const bodyParser = require('body-parser')
+// Use body-parser to parse body as text https://expressjs.com/en/resources/middleware/body-parser.html#bodyparsertextoptions
+app.use(bodyParser.text())
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-app.use(cors());
+app.use(cors())
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use('/public', express.static(`${process.cwd()}/public`))
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' })
+})
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+// Response with parsed as text request body
+app.post('/api/shorturl', function (req, res) {
+  res.json({ original_url: req.body, short_url: '1' })
+})
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`)
+})
